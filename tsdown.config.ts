11a50933de5b049c70abf404753b2ab0a4fff7a6
PuppyOne <zheng_xz@qq.com>
2026-06-13 19:38:53 +0800

    build: disable minify in tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: ['src/*.ts'],
   deps: { skipNodeModulesBundle: true },
-  minify: true,
   exports: true,
   publint: true,
   attw: true,
