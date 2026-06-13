a9b776dc4e86c4d0ae9a0d0e232ebbdc64045ddb
PuppyOne <zheng_xz@qq.com>
2026-06-13 19:25:13 +0800

    build: remove legacy exports suboption

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/*.ts'],
   deps: { skipNodeModulesBundle: true },
   minify: true,
-  exports: { legacy: true },
+  exports: true,
   publint: true,
   attw: true,
 });
